104d134cebafb732585ed4d5f501e5920d34af92
Abdella <abdella.shukur2@gmail.com>
2022-07-01 00:11:06 -0400

    All files included

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,27 @@
+import java.util.HashMap;
+import java.util.Map.Entry;
+
 public class Main {
     public static void main(String[] args) {
-        // your code here
+        String gradingMethod = "HEAVY"; 
+        StudentGradeTranslator gradeTranslator = new StudentGradeTranslator();
+
+        HashMap<String, String> studentGrades = new HashMap<String, String>();
+        studentGrades.put("Jamaal", "93");
+        studentGrades.put("Jennifer", "87");
+        studentGrades.put("Jules", "59");
+
+        // get all the student and their grades using each entry
+        System.out.println("List of students and their grades:");
+        for (Entry<String, String> studentGrade: studentGrades.entrySet()) {
+            System.out.println(studentGrade.getKey() + "'s grade is " +
+                    gradeTranslator.getLetterGrade(Integer.parseInt(studentGrade.getValue())));
+
+
+            /// This is the code to check for next Letter 
+
+            System.out.println(gradeTranslator.howManyForNextLetter(Integer.parseInt(studentGrade.getValue())) + " Points till next Letter. ");
+            // System.out.println("Passing grade status: " + gradeTranslator.isPassingGrade(Integer.parseInt(studentGrade.getValue())));
+        }
     }
 }
---
+++ src/main/java/StudentGradeTranslator.java
@@ -0,0 +1,125 @@
+
+public class StudentGradeTranslator {
+
+    GradeCalculator gradeCalculator;
+
+    public StudentGradeTranslator() {
+        this.gradeCalculator = new FlatCurveGradeCalculator();
+    }
+
+    public StudentGradeTranslator(String gradingMethod) {
+        if (gradingMethod == null) {
+            this.gradeCalculator = new FlatCurveGradeCalculator();
+        } else if (gradingMethod.equals("FLAT")) {
+            this.gradeCalculator = new FlatCurveGradeCalculator();
+        } else if (gradingMethod.equals("SLIGHT")) {
+            this.gradeCalculator = new SlightCurveGradeCalculator();
+        } else if (gradingMethod.equals("HEAVY")) {
+            this.gradeCalculator = new HeavyCurveGradeCalculator();
+        }
+    }
+
+    public String getLetterGrade(int numberGrade) {
+        return gradeCalculator.getLetterGrade(numberGrade);
+    }
+
+    public boolean isPassingGrade(int numberGrade) {
+        return gradeCalculator.isPassingGrade(numberGrade);
+    }
+
+    // the interface that dictates that methods each grade calculator
+    // implementation must support
+    interface GradeCalculator {
+        public String getLetterGrade(int numberGrade);
+        public boolean isPassingGrade(int numberGrade);
+    }
+
+
+    class FlatCurveGradeCalculator implements GradeCalculator {
+        public String getLetterGrade(int numberGrade) {
+            if (numberGrade < 60) {
+                return "F";
+            }
+            if (numberGrade < 70) {
+                return "D";
+            }
+            if (numberGrade < 80) {
+                return "C";
+            }
+            if (numberGrade < 90) {
+                return "B";
+            }
+            return "A";
+        }
+    
+    public boolean isPassingGrade(int numberGrade) {
+        if (numberGrade >= 60) return true;
+        return false;
+    }
+}
+
+class SlightCurveGradeCalculator implements GradeCalculator {
+    public String getLetterGrade(int numberGrade) {
+        if (numberGrade < 55) {
+           return "F";
+        }
+        if (numberGrade < 65) {
+           return "D";
+        }
+        if (numberGrade < 75) {
+           return "C";
+        }
+        if (numberGrade < 85) {
+           return "B";
+        }
+        return "A";
+     }
+
+        public boolean isPassingGrade(int numberGrade) {
+            if (numberGrade >= 55) return true;
+            return false;
+        }
+    }
+
+    class HeavyCurveGradeCalculator implements GradeCalculator {
+        public String getLetterGrade(int numberGrade) {
+            if (numberGrade < 50) {
+                return "F";
+            }
+            if (numberGrade < 60) {
+                return "D";
+            }
+            if (numberGrade < 70) {
+                return "C";
+            }
+            if (numberGrade < 80) {
+                return "B";
+            }
+            return "A";
+        }
+        public boolean isPassingGrade(int numberGrade) {
+            if (numberGrade >= 50) return true;
+            return false;
+        }
+    }
+
+     public int howManyForNextLetter(int numberGrade) {
+        if (numberGrade < 60) {
+            return (60-numberGrade);
+         }
+         if (numberGrade < 70) {
+            return (70-numberGrade);
+         }
+         if (numberGrade < 80) {
+            return (80-numberGrade);
+         }
+         if (numberGrade < 90) {
+            return (90-numberGrade);
+         }
+         int difference = (100-numberGrade);
+        return difference;
+    }
+
+
+
+};
